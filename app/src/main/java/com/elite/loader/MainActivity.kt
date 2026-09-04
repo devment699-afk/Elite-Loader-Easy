9c1a6fe115ff9eb51aa6ccb98663d1fcb0398e9b
ELITE-EASY <elite@easy.dev>
2026-09-04 05:12:55 +0000

    Fix R import for TEAM DARK

--- app/src/main/java/com/elite/loader/MainActivity.kt
+++ app/src/main/java/com/elite/loader/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.elite.EliteInstaller
+import com.teamdark.loader.R
 
 /**
  * ELITE LOADER - EASY UI
